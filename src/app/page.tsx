aecac8aca2357f520feaf0aae32b8ab7b03abcec
patarajarin <patara1919@gmail.com>
2025-01-13 01:49:13 +0700

    create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+'use client'
+import Homepage_article from "@/components/article/homePageDetail";
+import CLShomepage from "@/components/CLS/CLShomepage";
 
-
+// bg-[url('/assets/bghome.jpg')]
 export default function Home() {
   return (
-    <div className=" w-full pb-40 bg-[url('/assets/bghome.jpg')]">
+    <div className=" w-full pb-40 bg-gradient-to-tr from-gray-800 via-gray-500 to-gray-900 ">
     <div className="flex justify-center">
       <div className="bg-gray-800 mb-5">
-
+        <CLShomepage/>
       </div>
     </div>
-
+    <Homepage_article/>
   </div>
   );
 }

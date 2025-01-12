ac3f7dd4c0526d6782851e9aebb197b356102b74
patarajarin <patara1919@gmail.com>
2025-01-12 14:45:13 +0700

    edit config db

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export async function connect() {
     try{
 
-        mongoose.connect(process.env.MG_URL!);
+        mongoose.connect(process.env.MONGODB_URI!);
         const connection = mongoose.connection;
         connection.on("connected",()=>{
             console.log("MongoDB connected successfully!");

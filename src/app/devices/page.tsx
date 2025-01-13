abf83301ed8c25f871af39143d4cb6d2a5f61279
patarajarin <patara1919@gmail.com>
2025-01-14 00:58:09 +0700

    add styles loading devices page

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -6,17 +6,16 @@ import { Session } from "@/resource/model";
 import Body from "./Body";
 import { useRouter } from "next/navigation";
 export default function page() {
-  const router = useRouter()
+  const router = useRouter();
   const [session, setSession] = useState<Session | null>(null);
   const getUserSession = async () => {
     try {
       const res = await axios.get<{ data: Session }>("/api/users/me");
-      if (res.data.data){
-       setSession(res.data.data); 
-      }else{
-        router.push("/")
+      if (res.data.data) {
+        setSession(res.data.data);
+      } else {
+        router.push("/");
       }
-      
     } catch (error) {
       console.error("Failed to fetch session:", error);
     }
@@ -25,5 +24,19 @@ export default function page() {
     getUserSession();
   }, []);
 
-  return <div>{session && <Body session={session} />}</div>;
+  return (
+    <div>
+      {session ? (
+        <Body session={session} />
+      ) : (
+        <div className="bg-gray-700 py-20 px-5 lg:px-20 grid place-items-center">
+          <div className="my-10 border-2 lg:w-3/5 border-dotted rounded-md border-white grid place-items-center py-20">
+            <div className="text-xl lg:text-5xl font-bold text-gray-300 animate-pulse">
+              Loading...
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }

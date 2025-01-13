dea98dd19b497fa2f24336f69d2a6d7de4d38db0
patarajarin <patara1919@gmail.com>
2025-01-13 15:04:34 +0700

    edit styles login & logout

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
     try {
       const res = await axios.post("/api/users/login", user);
       if (res.data.success == true) {
-        toast.success("login success")
+        toast.success("login success");
         router.push("/devices");
       } else {
         console.log(res.data);
@@ -23,67 +23,61 @@ export default function Page() {
     } catch (error) {
       console.log(error instanceof Error ? error.message : "Unknown error");
       alert("login failed");
-    } 
+    }
   };
 
   return (
-    <div className="w-full grid place-items-center py-5">
-      <div className="flex justify-start px-5 w-full">
-        <button
-          className="px-10 py-1 rounded-md bg-slate-500 hover:bg-gray-600 text-white"
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          Back
-        </button>
-      </div>
-      <div className="w-fit grid place-items-start border border-gray-300 rounded-md mt-20 shadow-sm gap-2 px-10 py-5">
-        <div className="text-3xl text-black mb-2 text-center w-full">
-          Sign-In
-        </div>
-
-        <div className="flex gap-4">
-          <label className="w-[80px]" htmlFor="username">
-            Email
-          </label>
-          <input
-            type="text"
-            id="email"
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-            placeholder="email"
-            className="border px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
-          />
-        </div>
-        <div className="flex gap-4">
-          <label className="w-[80px]" htmlFor="username">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={user.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-            placeholder="password"
-            className="border px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
-          />
-        </div>
-        <div className="flex gap-10 my-4">
-          <button
-            className="px-10 py-1 bg-blue-500 hover:bg-blue-300 text-white rounded-md"
-            onClick={onLogin}
-          >
+    <div className="w-full grid place-items-center bg-gray-50 py-5">
+      <div className="relative  w-full max-w-2xl flex justify-center items-center">
+        <div className="absolute mix-blend-multiply filter blur-lg top-0 left-10 w-72 h-72 bg-sky-300 opacity-70 rounded-full animate-blob"></div>
+        <div className="absolute mix-blend-multiply filter blur-lg top-0 right-10 w-72 h-72  bg-yellow-300 opacity-60 rounded-full  animate-blob animation-delay-2000"></div>
+        <div className="absolute mix-blend-multiply filter blur-lg bottom-0 left-30 w-72 h-72 bg-pink-300 opacity-60 rounded-full  animate-blob animation-delay-4000"></div>
+        <div className="w-fit grid z-10 place-items-start bg-white  rounded-md my-10 shadow-sm gap-5 px-10 py-5">
+          <div className="text-3xl font-bold text-black mb-2 text-center w-full">
             Login
-          </button>
-          <button
-            className="px-10 py-1 hover:bg-gray-300 hover:text-white  text-blue-500 rounded-md"
-            onClick={() => {
-              router.push("/authentication/signup");
-            }}
-          >
-            Sign-Up
-          </button>
+          </div>
+          <div className="flex gap-4">
+            <label className="w-[80px]" htmlFor="username">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              placeholder="email"
+              className="border px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+            />
+          </div>
+          <div className="flex gap-4">
+            <label className="w-[80px]" htmlFor="username">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              placeholder="password"
+              className="border px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+            />
+          </div>
+          <div className="flex gap-10 my-4">
+            <button
+              className="px-10 py-1 bg-blue-500 hover:bg-blue-300 text-white rounded-md"
+              onClick={onLogin}
+            >
+              Login
+            </button>
+            <button
+              className="px-10 py-1 hover:bg-gray-300 hover:text-white  text-blue-500 rounded-md"
+              onClick={() => {
+                router.push("/authentication/signup");
+              }}
+            >
+              Sign-Up
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,27 @@ export default {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
+        blob:{
+          '0%':{
+            transform : "translate(10px,0px) scale(1)"
+          },
+          "33%": {
+            transform:"translate(50px,-60px) scale(1.2)"
+          },
+          "66%" :{
+            transform :"translate(-50px,60px) scale(0.8)",
+          },
+          "100%":{
+            transform : "translate(10px,0px) scale(1)",
+          }
+        },
       },
       animation: {
         fadeIn: 'fadeIn 2s ease-in-out',
         fastFade : 'fadeIn 1s ease-in-out',
+        fadeStep1: 'fadeIn 3s ease-in-out',
+        fadeStep2: 'fadeIn 4s ease-in-out',
+        blob:"blob 7s infinite",
       },
       colors: {
         background: "var(--background)",

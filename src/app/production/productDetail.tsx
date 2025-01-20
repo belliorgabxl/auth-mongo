ad76af0558d387e273f619ef5a2aff5ac0e39907
patarajarin <patara1919@gmail.com>
2025-01-20 09:26:10 +0700

    edit-card in production

--- src/app/production/productDetail.tsx
+++ src/app/production/productDetail.tsx
@@ -20,12 +20,12 @@ export default function ProductDetail({ isLoading }: Props) {
           className="lg:w-[300px] md:w-[250px] sm:w-[250px] w-44 rounded-l-lg object-cover "
         />
         <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
-          <h1 className="text-2xl  bg-gray-900 h-fit px-5 grid place-items-center py-1 rounded-md">
+          <h1 className="text-2xl  bg-gray-900 h-fit px-2 lg:px-5  grid place-items-center py-1 rounded-md">
             Product : Car
           </h1>
           <p className="text-lg">Board : ESP32</p>
           <p>Connection Type : Wi-fi 2.4 GHz</p>
-          <p className=" grid place-content-center rounded-md py-1 w-fit px-10 bg-gray-500 text-white text-xl">
+          <p className=" grid place-content-center rounded-md py-1 w-fit px-2 lg:px-10 bg-gray-500 text-white text-xl">
             Inventory : 4 pc.
           </p>
         </div>
@@ -45,12 +45,12 @@ export default function ProductDetail({ isLoading }: Props) {
         />
 
         <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
-          <div className="text-xl  bg-gray-900 h-fit px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
+          <div className="text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
             Product : Robotic Arm
           </div>
           <p className=" text-lg">Board : ESP32</p>
           <p>Connection Type : Wi-fi 2.4 GHz</p>
-          <p className=" grid place-content-center rounded-md py-1 w-fit px-10 bg-gray-500 text-white text-xl">
+          <p className=" grid place-content-center rounded-md py-1 w-fit px-2 lg:px-10 bg-gray-500 text-white text-xl">
             Inventory : 2 pc.
           </p>
         </div>
@@ -71,12 +71,12 @@ export default function ProductDetail({ isLoading }: Props) {
         />
 
         <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
-          <div className="text-xl  bg-gray-900 h-fit px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
+          <div className="text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
             Product : Auto Pump
           </div>
           <p className=" text-lg">Board : ESP32</p>
           <p>Connection Type : Wi-fi 2.4 GHz</p>
-          <p className=" grid place-content-center rounded-md py-1 w-fit px-10 bg-gray-500 text-white text-xl">
+          <p className=" grid place-content-center rounded-md py-1 w-fit px-2 lg:px-10 bg-gray-500 text-white text-xl">
             Inventory : 1 pc.
           </p>
         </div>
